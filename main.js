97168a573ffed38e1d9b3155ba5bd9a62abdf0fe
Paul Rouget <me@paulrouget.com>
2015-07-23 10:56:32 +0200

    B2

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ require.config({
   nodeIdCompat: true,
   paths: {
     browser: '../src/browser',
+    browser2: '../src/browser2',
     common: '../src/common',
     lang: '../src/lang',
     service: '../src/service',
     // http://facebook.github.io/react/
-    react: 'react/dist/react.min',
+    // react: 'react/dist/react.min',
+    react: 'react/dist/react',
     // http://facebook.github.io/immutable-js/
     // Because of the bug https://github.com/facebook/immutable-js/pull/297
     // we use forked version until it's uplifted.
@@ -30,4 +32,4 @@ require.config({
 });
 
 
-require(['browser/index']);
+require(['browser2/index']);
